3b64d08f36215f64c86fb2d20cf7b510c389e7c0
Francesco Ricci <francescosave.ricci@gmail.com>
2023-06-07 15:45:31 +0200

    run first test red passed

--- ClassTest.cs
+++ ClassTest.cs
@@ -1,13 +1,18 @@
 using Xunit;
-public class ClassTest
+public class BudgetAppTest
 {
 
     [Fact]
     public void Create_category_test(){
 
         Category category = new Category("food");
-        Category expetedCategory = new Category("food");
+        Category expetedCategory = new Category("clothing");
         Assert.Equal(expetedCategory,category);
     }
     
+}
+
+public record Category (string categorName)
+{
+
 }
